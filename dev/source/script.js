5bd351b333e1248a251618e77aa4431778f379e8
Fireye04 <Fireye04@users.noreply.github.com>
2025-12-01 02:21:24 +0000

    deploy: e69412fb67b5cb8a6d3b396929dd6a68000c3ad9

--- dev/source/script.js
+++ dev/source/script.js
@@ -6,11 +6,48 @@ config.ui.stowBarInitially = true;
 config.history.controls = false;
 */
 
-function getWeather(options = ["rainy", "sunny"]) {
+/********GLOBALS********/
+window.endureCount = 0;
+
+
+/********STRUCTS********/
+class thingAtCamp {
+    constructor(itemName, rainyDesc, sunnyDesc, enduredRainyDesc, enduredSunnyDesc) {
+        this.name = itemName;
+        this.rainyDesc = rainyDesc;
+        this.sunnyDesc = sunnyDesc;
+        this.enduredRainyDesc = enduredRainyDesc;
+        this.enduredSunnyDesc = enduredSunnyDesc;
+    }
+    getDescription(weather, endured){
+
+    }
+}
+
+
+
+/********WEATHER********/
+function getWeather(options) {
     const roll = Math.floor(Math.random() * (options.length-1) + 0.5);
 
     console.log(roll);
     return options[roll];
 }
-
 window.getWeather = (options = ["rainy", "sunny"]) => getWeather(options);
+
+
+/********THINGS AT CAMP********/
+var thingsAtCampDefault = ["campfire", ""];
+var thingsAtCamp = [];
+
+function setThingsAtCamp(things, noDefaults) {
+    if (noDefaults){
+    thingsAtCamp = things;
+
+    } else {
+        thingsAtCamp = thingsAtCampDefault;
+        thingsAtCamp.concat(things);
+    }
+}
+
+window.setThingsAtCamp = (things = thingsAtCampDefault, noDefaults = false) => setThingsAtCamp(things, noDefaults);
